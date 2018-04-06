aef8117e08ab8d2abd73926358ca929065a4d351
Elias Ojala <git@eliasojala.me>
2018-04-06 22:50:36 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "env": {
+    "browser": true
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* Add your JavaScript code here */
+
+$( "#helloWorld" ).html( "Hello world from <strong>JS</strong>!" );
+
+$( "#helloWorldButton" ).click(function() {
+  alert("You clicked me!");
+});
